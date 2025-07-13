4ab41ddce9890851594fde3179a5438b5d8694ee
vgzenda <57106623+vgzenda@users.noreply.github.com>
2025-07-13 17:13:04 +0000

    Deploying to gh-pages from @ vgzenda/vgzenda.github.io@8f5fffaab12bd23356fe1c12c2fb4c9171cb79a9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -165,17 +165,6 @@ ninja.data = [{
             window.location.href = "/blog/2024/echarts/";
           
         },
-      },{id: "post-a-post-with-chart-js",
-        
-          title: "a post with chart.js",
-        
-        description: "this is what included chart.js code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/chartjs/";
-          
-        },
       },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
